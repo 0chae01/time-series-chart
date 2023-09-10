d4951e29c0a26d1683f0436618033c9b7a993aaa
youngchae <010101chae@gmail.com>
2023-09-11 00:09:55 +0900

    feat: 데이터타입, 차트데이터타입 선언

---
+++ src/types/data.ts
@@ -0,0 +1,14 @@
+export interface dataPieceType {
+  id: string;
+  value_area: number;
+  value_bar: number;
+}
+
+export type responseDataType = Record<string, dataPieceType>;
+
+export interface chartDataType {
+  id: string;
+  value_area: number;
+  value_bar: number;
+  time: string;
+}
