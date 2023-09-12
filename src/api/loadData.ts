aed88748a152e8c3fac8d94d6dcef2b18e4562d5
youngchae <010101chae@gmail.com>
2023-09-12 23:35:41 +0900

    refactor: API_BASE_URL 상수화

--- src/api/loadData.ts
+++ src/api/loadData.ts
@@ -1,8 +1,9 @@
+import { API_BASE_URL } from "@/constants/api";
 import getChartData from "@/utils/getChartData";
 
 const loadData = async () => {
   try {
-    const response = await fetch("/data.json");
+    const response = await fetch(API_BASE_URL);
     if (!response.ok) {
       throw new Error("데이터 로딩에 실패했습니다.");
     }
