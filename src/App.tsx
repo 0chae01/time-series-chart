10821a9455a0283e68a8b6c257460333222e1284
youngchae <010101chae@gmail.com>
2023-09-12 20:28:04 +0900

    refactor: App.tsx에서 데이터 호출 및 로딩처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { chartDataType } from "@/types/data";
 import Chart from "@/components/Chart";
 
 function App() {
-  return <Chart />;
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState<chartDataType[]>();
+
+  const fetchedData = useLoaderData() as chartDataType[];
+
+  useEffect(() => {
+    setData(fetchedData);
+    setIsLoading(false);
+  }, []);
+
+  if (!data) return <div>데이터를 불러오는 데 실패했습니다.</div>;
+
+  return <>{isLoading ? <div>Loading...</div> : <Chart data={data} />}</>;
 }
 
 export default App;
--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
-import styled from "styled-components";
-import { useLoaderData } from "react-router-dom";
+import { useState } from "react";
 import { chartDataType } from "@/types/data";
 import {
   ComposedChart,
@@ -23,17 +21,12 @@ import {
   CategoricalChartState,
 } from "recharts/types/chart/generateCategoricalChart";
 
-const Chart = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState<chartDataType[]>();
+interface ChartProps {
+  data: chartDataType[];
+}
+
+const Chart = ({ data }: ChartProps) => {
   const [hiddenValue, setHiddenValue] = useState("");
-
-  const fetchedData = useLoaderData() as chartDataType[];
-
-  useEffect(() => {
-    setData(fetchedData);
-    setIsLoading(false);
-  }, []);
 
   const { curQueryData, toggleFilter } = useQueryParams("region");
 
@@ -60,96 +53,80 @@ const Chart = () => {
 
   if (!data) return null;
   return (
-    <Container>
-      {isLoading ? (
-        "Loading..."
-      ) : (
-        <ResponsiveContainer width={"100%"} height={600}>
-          <ComposedChart
-            data={data}
-            margin={{
-              top: 20,
-              right: 20,
-              bottom: 20,
-              left: 10,
-            }}
-            onClick={onClick as CategoricalChartFunc}
-          >
-            <CartesianGrid stroke="#f5f5f5" />
-            <XAxis
-              dataKey="time"
-              width={30}
-              height={70}
-              tickMargin={5}
-              tickSize={15}
+    <ResponsiveContainer width={"100%"} height={600}>
+      <ComposedChart
+        data={data}
+        margin={{
+          top: 20,
+          right: 20,
+          bottom: 20,
+          left: 10,
+        }}
+        onClick={onClick as CategoricalChartFunc}
+      >
+        <CartesianGrid stroke="#f5f5f5" />
+        <XAxis
+          dataKey="time"
+          width={30}
+          height={70}
+          tickMargin={5}
+          tickSize={15}
+        />
+        <YAxis
+          yAxisId="left"
+          label={{
+            value: "value_area",
+            angle: -90,
+            position: "insideLeft",
+            offset: 1,
+          }}
+          domain={[0, 200]}
+        />
+        <YAxis
+          yAxisId="right"
+          orientation="right"
+          label={{
+            value: "value_bar",
+            angle: -90,
+            position: "insideRight",
+            offset: -10,
+          }}
+          domain={[0, 20000]}
+        />
+        <Tooltip content={<CustomToolTip />} />
+        <Bar
+          dataKey="value_bar"
+          barSize={20}
+          fill="#ffb700"
+          yAxisId="right"
+          hide={hiddenValue === "value_bar"}
+        >
+          {data.map((entry, index) => (
+            <Cell
+              fill={curQueryData.includes(entry.id) ? "#ff6200" : "#ffb700"}
+              key={`cell-${index}`}
             />
-            <YAxis
-              yAxisId="left"
-              label={{
-                value: "value_area",
-                angle: -90,
-                position: "insideLeft",
-                offset: 1,
-              }}
-              domain={[0, 200]}
-            />
-            <YAxis
-              yAxisId="right"
-              orientation="right"
-              label={{
-                value: "value_bar",
-                angle: -90,
-                position: "insideRight",
-                offset: -10,
-              }}
-              domain={[0, 20000]}
-            />
-            <Tooltip content={<CustomToolTip />} />
-            <Bar
-              dataKey="value_bar"
-              barSize={10}
-              fill="#ffb700"
-              yAxisId="right"
-              hide={hiddenValue === "value_bar"}
-            >
-              {data.map((entry, index) => (
-                <Cell
-                  fill={curQueryData.includes(entry.id) ? "#ff6200" : "#ffb700"}
-                  key={`cell-${index}`}
-                />
-              ))}
-            </Bar>
-            <Area
-              type="monotone"
-              dataKey="value_area"
-              fill="#b3c5ff"
-              stroke="#277fe4"
-              yAxisId="left"
-              legendType="circle"
-              dot={<CustomDot />}
-              activeDot={{ fill: "red" }}
-              hide={hiddenValue === "value_area"}
-            />
-            <Legend
-              height={50}
-              margin={{ top: 100 }}
-              onClick={(data) => toggleValueType(data)}
-            />
-          </ComposedChart>
-        </ResponsiveContainer>
-      )}
-    </Container>
+          ))}
+        </Bar>
+        <Area
+          type="monotone"
+          dataKey="value_area"
+          fill="#b3c5ff"
+          stroke="#277fe4"
+          yAxisId="left"
+          legendType="circle"
+          dot={<CustomDot />}
+          activeDot={{ fill: "red" }}
+          hide={hiddenValue === "value_area"}
+        />
+        <Legend
+          height={50}
+          margin={{ top: 100 }}
+          onClick={(data) => toggleValueType(data)}
+        />
+      </ComposedChart>
+    </ResponsiveContainer>
   );
 };
 
 export default Chart;
-
-const Container = styled.div`
-  max-width: 1440px;
-  margin-inline: auto;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 0.25rem;
-`;
