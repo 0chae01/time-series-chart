3ae6983b999d86f86a0155bd03c87655e57b44ab
youngchae <010101chae@gmail.com>
2023-09-12 20:26:47 +0900

    style: import alias 통일

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
-import loadData from "./api/loadData";
-import App from "./App";
+import loadData from "@/api/loadData";
+import App from "@/App";
 
 export const router = createBrowserRouter([
   {
