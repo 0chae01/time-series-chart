fd29c7c5dbc85c926afa69265fce0c98fb1e1b25
youngchae <010101chae@gmail.com>
2023-09-12 21:53:52 +0900

    remove: changeTimeFormat 함수 삭제

--- src/utils/changeTimeFormat.ts
+++
@@ -1,8 +0,0 @@
-export const changeTimeFormat = (time: Date) => {
-  const hours = time.getHours();
-  const minutes = time.getMinutes();
-  const seconds =
-    time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
-
-  return `${hours}:${minutes}:${seconds}`;
-};
